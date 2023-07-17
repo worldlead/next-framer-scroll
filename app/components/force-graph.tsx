b0cb954f6bb6e3f8b0de37e02d41d593c60fd7cb
worldlead <procosep@gmail.com>
2023-07-17 15:12:59 -0400

    fix

--- app/components/force-graph.tsx
+++ app/components/force-graph.tsx
@@ -17,21 +17,16 @@ export default function ForceGraph({ className }: ForceGraphProps) {
 			.then(data => setGraphData(data));
 	}, []);
 
-	const handleMouseMove = (event: MouseEvent) => {
+	const handleMouseMove = (event) => {
 		const x = event.clientX;
 		const y = event.clientY;
 
 		const windowWidth = window.innerWidth;
 		const windowHeight = window.innerHeight;
 
-		console.log('Window dimensions:', windowWidth, windowHeight);
-		console.log('Mouse position:', x, y);
-
 		// normalize the mouse coordinates to range [-1, 1] and invert y-axis
 		const nx = 2*(x / windowWidth - 0.5);
 		const ny = -2*(y / windowHeight - 0.5);
-
-		console.log('Normalized coordinates:', nx, ny);
 
 		// compute the new camera position, adjust the 10 to control orbit radius
 		const newCameraPosition = {
@@ -40,12 +35,8 @@ export default function ForceGraph({ className }: ForceGraphProps) {
 			z: 3  // keep z constant to maintain a horizontal orbit
 		};
 
-		console.log('New camera position:', newCameraPosition);
-
-		if (fgRef.current) {
-			const graph = fgRef.current as typeof ForceGraph3D;
-			graph.cameraPosition(pos);
-		  }
+		// update the camera position
+		fgRef.current.cameraPosition(newCameraPosition);
 	}
 
 	return graphData && (
