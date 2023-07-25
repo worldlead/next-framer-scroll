3be7009087b5a3aa16635153d6a92b7d9bba3d58
worldlead <procosep@gmail.com>
2023-07-24 22:01:45 -0400

    auth stuff

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,63 +2,134 @@
 import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
-import AuthBasic from '../components/auth';
-import { Card } from "../components/card";
+import Login from "../components/auth";
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/Fylo_Labs",
-		label: "Twitter",
-		handle: "@Fylo_Labs",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:info@fylo.io",
-		label: "Email",
-		handle: "info@fylo.io",
-	},
-	// {
-	// 	icon: <Github size={20} />,
-	// 	href: "https://github.com/chronark",
-	// 	label: "Github",
-	// 	handle: "chronark",
-	// },
-];
+// export default function Example() {
+// 	return (
+// 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+// 			<Navigation />
+// 			<Login />
+// 		</div>
+// 	);
+// }
+
+
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-2 lg:gap-16">
-					{socials.map((s) => (
-						<Card>
-							<Link
-								href={s.href}
-								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-							>
-								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
-								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-									{s.icon}
-								</span>{" "}
-								<div className="z-10 flex flex-col items-center">
-									<span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-										{s.handle}
-									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-										{s.label}
-									</span>
-								</div>
-							</Link>
-						</Card>
-					))}
-				</div>
+		<div>
+			{/* <Navigation /> */}
+			<div className="bg-white container flex items-center justify-center min-h-screen px-4 mx-auto">
+				<Login />
 			</div>
 		</div>
 	);
 }
+
+
+
+
+
+// export default function Example() {
+// 	return (
+// 		<div className="relative pb-16">
+// 			<Navigation />
+// 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+// 				<div className="max-w-2xl mx-auto lg:mx-0">
+// 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+// 						Features
+// 					</h2>
+// 					<p className="mt-4 text-zinc-400">
+// 						Fylo is dedicated to augmenting ideation.
+// 					</p>
+// 				</div>
+// 				<div className="w-full h-px bg-zinc-800" />
+
+// 				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+// 					<Card>
+// 						<Link href={`/features/${featured.slug}`}>
+// 							<article className="relative w-full h-full p-4 md:p-8">
+// 								<div className="flex items-center justify-between gap-2">
+// 									<div className="text-xs text-zinc-100">
+// 										{featured.date ? (
+// 											<time dateTime={new Date(featured.date).toISOString()}>
+// 												{Intl.DateTimeFormat(undefined, {
+// 													dateStyle: "medium",
+// 												}).format(new Date(featured.date))}
+// 											</time>
+// 										) : (
+// 											<span>SOON</span>
+// 										)}
+// 									</div>
+// 									<span className="flex items-center gap-1 text-xs text-zinc-500">
+// 										<Eye className="w-4 h-4" />{" "}
+// 										{Intl.NumberFormat("en-US", { notation: "compact" }).format(
+// 											views[featured.slug] ?? 0,
+// 										)}
+// 									</span>
+// 								</div>
+
+// 								<h2
+// 									id="featured-post"
+// 									className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+// 								>
+// 									{featured.title}
+// 								</h2>
+// 								<p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+// 									{featured.description}
+// 								</p>
+// 								<div className="absolute bottom-4 md:bottom-8">
+// 									<p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+// 										Read more <span aria-hidden="true">&rarr;</span>
+// 									</p>
+// 								</div>
+// 							</article>
+// 						</Link>
+// 					</Card>
+
+// 					<div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+// 						{[top2, top3].map((feature) => (
+// 							<Card key={feature.slug}>
+// 								<Article feature={feature} views={views[feature.slug] ?? 0} />
+// 							</Card>
+// 						))}
+// 					</div>
+// 				</div>
+// 				<div className="hidden w-full h-px md:block bg-zinc-800" />
+
+// 				<div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+// 					<div className="grid grid-cols-1 gap-4">
+// 						{sorted
+// 							.filter((_, i) => i % 3 === 0)
+// 							.map((feature) => (
+// 								<Card key={feature.slug}>
+// 									<Article feature={feature} views={views[feature.slug] ?? 0} />
+// 								</Card>
+// 							))}
+// 					</div>
+// 					<div className="grid grid-cols-1 gap-4">
+// 						{sorted
+// 							.filter((_, i) => i % 3 === 1)
+// 							.map((feature) => (
+// 								<Card key={feature.slug}>
+// 									<Article feature={feature} views={views[feature.slug] ?? 0} />
+// 								</Card>
+// 							))}
+// 					</div>
+// 					<div className="grid grid-cols-1 gap-4">
+// 						{sorted
+// 							.filter((_, i) => i % 3 === 2)
+// 							.map((feature) => (
+// 								<Card key={feature.slug}>
+// 									<Article feature={feature} views={views[feature.slug] ?? 0} />
+// 								</Card>
+// 							))}
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
