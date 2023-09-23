6b8de1a86c5a4dfb0021faad37aa111238b4ec04
worldlead <procosep@gmail.com>
2023-09-23 16:03:20 +0600

    version update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,39 +2,46 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Navbar() {
+  const navLinks = [
+    { text: "about", href: "/about" },
+    { text: "features", href: "/features" }, // Added "features" link
+  ];
+
   return (
     <>
-      <nav className="px-14 text-black h-16 fixed top-16 w-full transition-colors duration-500 z-40">
-        <ul className="flex justify-between items-center">
-          <li>
-            <Link
-              href="/about"
-              className="inline-block -mx-2 -my-1 px-2 py-1 text-sm"
-            >
-              about
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image
-                src="/assets/logo.svg"
-                alt="Logo"
-                width="71"
-                height="30"
-                className=""
-              />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/login"
-              className="text-sm px-10 py-3 rounded-[10px] shadow-lg bg-white"
-            >
-              Login
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="flex justify-between items-center px-14 text-black h-16 fixed top-8 w-full transition-colors duration-500 z-40">
+        <nav>
+          <ul className="flex items-center space-x-4">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <Link
+                  href={link.href}
+                  className="inline-block -mx-2 -my-1 px-2 py-1 text-sm"
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <Link href="/">
+          <Image src="/assets/logo.svg" alt="Logo" width="71" height="30" />
+        </Link>
+
+        <nav>
+          <ul className="flex items-center">
+            <li>
+              <Link
+                href="/login"
+                className="text-sm px-10 py-3 rounded-[10px] shadow-lg bg-white"
+              >
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
--- app/features/article.tsx
+++ app/features/article.tsx
@@ -3,38 +3,38 @@ import Link from "next/link";
 import { Eye, View } from "lucide-react";
 
 type Props = {
-	feature: Feature;
-	views: number;
+  feature: Feature;
+  views: number;
 };
 
 export const Article: React.FC<Props> = ({ feature, views }) => {
-	return (
-		<Link href={`/features/${feature.slug}`}>
-			<article className="p-4 md:p-8">
-				<div className="flex justify-between gap-2 items-center">
-					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
-						{feature.date ? (
-							<time dateTime={new Date(feature.date).toISOString()}>
-								{Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
-									new Date(feature.date),
-								)}
-							</time>
-						) : (
-							<span>SOON</span>
-						)}
-					</span>
-					<span className="text-zinc-500 text-xs  flex items-center gap-1">
-						<Eye className="w-4 h-4" />{" "}
-						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
-					</span>
-				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{feature.title}
-				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-					{feature.description}
-				</p>
-			</article>
-		</Link>
-	);
+  return (
+    <Link href={`/features/${feature.slug}`}>
+      <article className="p-4 md:p-8">
+        <div className="flex justify-between gap-2 items-center">
+          <span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
+            {feature.date ? (
+              <time dateTime={new Date(feature.date).toISOString()}>
+                {Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
+                  new Date(feature.date)
+                )}
+              </time>
+            ) : (
+              <span>SOON</span>
+            )}
+          </span>
+          <span className="text-zinc-500 text-xs  flex items-center gap-1">
+            <Eye className="w-4 h-4" />{" "}
+            {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+          </span>
+        </div>
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+          {feature.title}
+        </h2>
+        <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+          {feature.description}
+        </p>
+      </article>
+    </Link>
+  );
 };
